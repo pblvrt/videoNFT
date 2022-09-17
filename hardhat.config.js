574d30f3aa478ff30a065236612c43d2d2b5a086
pabloVoorvaart <pablov@graficadirecta.com>
2022-09-17 18:32:38 +0200

    intial config

---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("./scripts/deploy.js");
+
+const { ALCHEMY_KEY, ACCOUNT_PRIVATE_KEY } = process.env;
+
+module.exports = {
+   solidity: "0.8.1",
+   defaultNetwork: "rinkeby",
+   networks: {
+    hardhat: {},
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },  
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },
+    ethereum: {
+      chainId: 1,
+      url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+      accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
+    },
+  },
+}
