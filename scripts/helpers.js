90703d57ca7c1df62ae8ab10a79a305140a1bb3e
pabloVoorvaart <pablov@graficadirecta.com>
2022-09-17 20:11:04 +0200

    first contract iteration

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,14 +1,39 @@
-const { task } = require("hardhat/config");
-const { getAccount } = require("./helpers");
+const { ethers } = require("ethers");
+const { getContractAt } = require("@nomiclabs/hardhat-ethers/internal/helpers");
 
 
-task("check-balance", "Prints out the balance of your account").setAction(async function (taskArguments, hre) {
+// Helper method for fetching environment variables from .env
+function getEnvVariable(key, defaultValue) {
+    if (process.env[key]) {
+        return process.env[key];
+    }
+    if (!defaultValue) {
+        throw `${key} is not defined and no default value was provided`;
+    }
+    return defaultValue;
+}
+
+// Helper method for fetching a connection provider to the Ethereum network
+function getProvider() {
+    return ethers.getDefaultProvider(getEnvVariable("NETWORK", "rinkeby"), {
+        alchemy: getEnvVariable("ALCHEMY_KEY"),
+    });
+}
+
+// Helper method for fetching a wallet account using an environment variable for the PK
+function getAccount() {
+    return new ethers.Wallet(getEnvVariable("ACCOUNT_PRIVATE_KEY"), getProvider());
+}
+
+// Helper method for fetching a contract instance at a given address
+function getContract(contractName, hre) {
     const account = getAccount();
-    console.log(`Account balance for ${account.address}: ${await account.getBalance()}`);
-});
+    return getContractAt(hre, contractName, getEnvVariable("NFT_CONTRACT_ADDRESS"), account);
+}
 
-task("deploy", "Deploys the NFT.sol contract").setAction(async function (taskArguments, hre) {
-    const nftContractFactory = await hre.ethers.getContractFactory("NFT", getAccount());
-    const nft = await nftContractFactory.deploy();
-    console.log(`Contract deployed to address: ${nft.address}`);
-});
+module.exports = {
+    getEnvVariable,
+    getProvider,
+    getAccount,
+    getContract,
+}
