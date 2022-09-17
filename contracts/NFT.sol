9a3d6a1621e8ab640018525c28b59228a59ad046
pabloVoorvaart <pablov@graficadirecta.com>
2022-09-17 23:32:40 +0200

    finished contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -3,24 +3,38 @@ pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/PullPayment.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFT is ERC721 {
+
+contract NFT is ERC721, PullPayment, Ownable {
   using Counters for Counters.Counter;
+
+  // Constants
+  uint256 public constant TOTAL_SUPPLY = 10_000;
 
   Counters.Counter private currentTokenId;
 
   /// @dev Base token URI used as a prefix by tokenURI().
   string public baseTokenURI;
 
-  constructor() ERC721("videoNFT", "NFT") {
+  constructor() ERC721("NFTTutorial", "NFT") {
     baseTokenURI = "";
   }
 
-  function mintTo(address recipient) public returns (uint256) {
+  function mintTo(address recipient) public payable returns (uint256) {
+    uint256 tokenId = currentTokenId.current();
+    require(tokenId < TOTAL_SUPPLY, "Max supply reached");
+
     currentTokenId.increment();
     uint256 newItemId = currentTokenId.current();
     _safeMint(recipient, newItemId);
     return newItemId;
+  }
+
+function tokenURI(uint256 tokenId) override public view returns (string memory) {
+    require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+    return baseTokenURI;
   }
 
   /// @dev Returns an URI for a given token ID
@@ -29,7 +43,12 @@ contract NFT is ERC721 {
   }
 
   /// @dev Sets the base token URI prefix.
-  function setBaseTokenURI(string memory _baseTokenURI) public {
+  function setBaseTokenURI(string memory _baseTokenURI) public onlyOwner {
     baseTokenURI = _baseTokenURI;
+  }
+
+  /// @dev Overridden in order to make it an onlyOwner function
+  function withdrawPayments(address payable payee) public override onlyOwner virtual {
+      super.withdrawPayments(payee);
   }
 }
